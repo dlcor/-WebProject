f30da68064730c40b36e1fcaeb99608384597da3
dicor <165188939+dlcor@users.noreply.github.com>
2024-04-22 16:21:14 +0300

    Added files

---
+++ LastWeb/Controllers/HommeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LastWeb.Controllers
+{
+    public class HommeController : Controller
+    {
+        // GET: Homme
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        public ActionResult Shop()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
